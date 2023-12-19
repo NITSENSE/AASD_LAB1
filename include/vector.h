b83e4af34a4bea2fd1334eb62d6d57815f65f6ac
NITSENSE KO <volodia.kostsoff@yandex.ru>
2023-12-19 09:57:56 +0400

    corrected

--- include/vector.h
+++ include/vector.h
@@ -1,17 +1,19 @@
+#pragma once
 #include <iostream>
 #include <complex>
 #include <random>
 #include <typeinfo> 
-
+#include <math.h>
 template<typename T>
 class Vector{
-    T** _coordinates;
+    T* _coordinates;
     int _dimension; 
 public:
     Vector() : _coordinates(nullptr), _dimension(0) {};
     Vector(int size, T value);
     Vector(int size, T bottom, T top);
     Vector(const Vector& copy);
+    ~Vector();
     void Swap(Vector& other) noexcept;
     const T& operator[](int index) const;
     T& operator[](int index);
@@ -20,11 +22,25 @@ public:
     Vector<T>& operator-=(const Vector<T>& other);
     Vector<T>& operator+(const Vector<T>& other) const;
     Vector<T>& operator-(const Vector<T>& other) const;
-    T& operator*(const Vector<T>& other) const;
+    T operator*(const Vector<T>& other) const; //было Т&
     Vector<T>& operator*=(const T& scalar);
     Vector<T>& operator*(const T& scalar) const; 
     Vector<T>& operator/=(const T& scalar);
     Vector<T>& operator/(const T& scalar) const;
+    bool operator==(const Vector& other) const;
+    bool operator!=(const Vector& other) const { return !(*this == other); }
+    friend std::ostream& operator<<(std::ostream& os, const Vector& vector) {
+        os << "(";
+        for (int i = 0; i < vector._dimension; ++i) {
+            os << vector._coordinates[i];
+            if (i != vector._dimension - 1) {
+                os << ", ";
+            }
+        }
+        os << ")";
+        return os;
+    }
+    double abs() const;
     void print();
 };
 
@@ -32,4 +48,12 @@ template <typename T>
 Vector<T> operator*(const T& scalar, Vector<T>& vec) {
     Vector<T> temp = vec;
     return temp *= scalar;
+}
+
+template <typename T>
+double area(const Vector<T>& other, const Vector<T>& other2) {
+    double corner_cos = (other2 * other) / (other2.abs() * other.abs());
+    double corner_sin = sqrt(1 - (corner_cos * corner_cos));
+    double left_piece = other2.abs() * other.abs();
+    return left_piece * corner_sin * 0.5; 
 }
--- src/vector.cc
+++ src/vector.cc
@@ -1,80 +1,80 @@
-#pragma once
+
 #include "../include/vector.h"
 
 template<typename T>
-Vector<T>::Vector(int size, T value) : _dimension(size) {
-        _coordinates = new T * [size];
+Vector<T>::Vector(int size, T value) : _dimension(size), _coordinates(new T[size]) {
         for(int i = 0; i < size; i++){
-            _coordinates[i] = new T (value);
+            _coordinates[i] = value;
         }
     }
 template<typename T>
-Vector<T>::Vector(int size, T bottom, T top) : _dimension(size){
-    _coordinates = new T*[size];
+Vector<T>::Vector(int size, T bottom, T top) : _dimension(size), _coordinates(new T[size]) {
     std::random_device rd;
     std::mt19937 gen(rd());
 
     if constexpr (std::is_integral_v<T>) {
         std::uniform_int_distribution<T> dist(bottom, top);
         for (int i = 0; i < size; i++) {
-            _coordinates[i] = new T(dist(gen));
+            _coordinates[i] = dist(gen);
         }
     }
 
     else if constexpr (std::is_floating_point_v<T>) {
         std::uniform_real_distribution<T> dist(bottom, top);
         for (int i = 0; i < size; i++) {
-            _coordinates[i] = new T(dist(gen));
+            _coordinates[i] = dist(gen);
         }
     }
 }
 
 template<>
-Vector<std::complex<double>>::Vector(int size, std::complex<double> bottom, std::complex<double> top) : _dimension(size) {
-    _coordinates = new std::complex<double>*[size];
+Vector<std::complex<double>>::Vector(int size, std::complex<double> bottom, std::complex<double> top) : _dimension(size), _coordinates(new std::complex<double>[size]) {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<double> dist_r(bottom.real(), top.real());
     std::uniform_real_distribution<double> dist_i(bottom.imag(), top.imag());
-    std::cout << bottom.real()<< top.real() << std::endl;
     for (int i = 0; i < size; i++) {
-        _coordinates[i] = new std::complex<double>(dist_r(gen), dist_i(gen));
+        _coordinates[i] = std::complex<double>(dist_r(gen), dist_i(gen));
     }
 }
 
 template<>
-Vector<std::complex<float>>::Vector(int size, std::complex<float> bottom, std::complex<float> top) : _dimension(size) {
-    _coordinates = new std::complex<float>*[size];
+Vector<std::complex<float>>::Vector(int size, std::complex<float> bottom, std::complex<float> top) : _dimension(size), _coordinates(new std::complex<float>[size]) {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<float> dist_r(bottom.real(), top.real());
     std::uniform_real_distribution<float> dist_i(bottom.imag(), top.imag());
-    std::cout << bottom.real() << top.real() << std::endl;
     for (int i = 0; i < size; i++) {
-        _coordinates[i] = new std::complex<float>(dist_r(gen), dist_i(gen));
+        _coordinates[i] = std::complex<float>(dist_r(gen), dist_i(gen));
     }
+}
+
+template<typename T>
+Vector<T>::~Vector() {
+    delete[] _coordinates;
 }
 
 template<typename T>
 const T& Vector<T>::operator[](int index) const {
-    if (index >= 0 || _dimension > index) {
-        return *_coordinates[index];
+    if (index >= _dimension) {
+        throw std::out_of_range("Index out of range");
     }
+    return _coordinates[index];
 }
 
 template<typename T>
 T& Vector<T>::operator[](int index) {
-    if (index >= 0 || _dimension > index)
-        return *_coordinates[index];
+    if (index >= _dimension) {
+        throw std::out_of_range("Index out of range");
+    }
+    return _coordinates[index];
 }
 
 template<typename T>
-Vector<T>::Vector(const Vector<T>& copy) :
-    _coordinates(new T* [copy._dimension]),
-    _dimension(copy._dimension)
-{
-    for (int i = 0; i < _dimension; ++i)
-        _coordinates[i] = new T(*copy._coordinates[i]);
+Vector<T>::Vector(const Vector& copy) : _dimension(copy._dimention), _coordinates(new T[copy._dimention]) {
+    for (int i = 0; i < _dimension; ++i) {
+        _coordinates[i] = copy._coordinates[i];
+    }
 }
 
 template<typename T>
@@ -86,7 +86,7 @@ void Vector<T>::Swap(Vector<T>& other) noexcept
 
 template<typename T>
 Vector<T>& Vector<T>::operator=(Vector<T> other) {
-    Swap(other);
+    Swap(other);     
     return *this;
 }
 
@@ -97,7 +97,7 @@ Vector<T>& Vector<T>::operator+=(const Vector<T> &other) {
         throw std::invalid_argument("Vectors dimensions do not match");
     }
     for (int i = 0; i < _dimension; i++) {
-        *_coordinates[i] += *other._coordinates[i];
+        _coordinates[i] += other._coordinates[i];
     }
     return *this;
 }
@@ -109,7 +109,7 @@ Vector<T>& Vector<T>::operator-=(const Vector<T>& other) {
         throw std::invalid_argument("Vectors dimensions do not match");
     }
     for (int i = 0; i < _dimension; i++) {
-        *_coordinates[i] -= *other._coordinates[i];
+        _coordinates[i] -= other._coordinates[i];
     }
     return *this;
 }
@@ -136,13 +136,13 @@ Vector<T>& Vector<T>::operator-(const Vector<T>& other) const {
 
 
 template<typename T>
-T& Vector<T>::operator*(const Vector<T>& other) const {
+T Vector<T>::operator*(const Vector<T>& other) const {
     if (_dimension != other._dimension) {
         throw std::invalid_argument("Vectors dimensions do not match");
     }
     T result = 0;
     for (int i = 0; i < _dimension; i++) {
-        result += *_coordinates[i] * *other._coordinates[i];
+        result += _coordinates[i] * other._coordinates[i];
     }
     return result;
 }
@@ -150,7 +150,7 @@ T& Vector<T>::operator*(const Vector<T>& other) const {
 template<typename T>
 Vector<T>& Vector<T>::operator*=(const T& scalar) {
     for (int i = 0; i < _dimension; i++) {
-        *_coordinates[i] *= scalar;
+        _coordinates[i] *= scalar;
     }
     return *this;
 }
@@ -167,7 +167,7 @@ Vector<T>& Vector<T>::operator/=(const T& scalar) {
         throw std::invalid_argument("divide by zero");
     }
     for (int i = 0; i < _dimension; i++) {
-        *_coordinates[i] /= scalar;
+        _coordinates[i] /= scalar;
     }
     return *this;
 }
@@ -181,11 +181,48 @@ Vector<T>& Vector<T>::operator/(const T& scalar) const {
     return temp /= scalar;
 }
 
+template<typename T>
+bool Vector<T>::operator==(const Vector& other) const {
+    if (_dimension != other._dimension) {
+        return false;
+    }
+
+    for (int i = 0; i < _dimension; ++i) {
+        if (_coordinates[i] != other._coordinates[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 template<typename T>
 void Vector<T>::print() {
     for (int i = 0; i < _dimension; i++) {
-        std::cout << *_coordinates[i] << std::endl;
+        std::cout<<"Value " << i + 1 << ": " << *_coordinates[i] << std::endl;
     }
 }
 
+template<typename T>
+double Vector<T>::abs() const {
+    T temp = 0;
+    for (int i = 0; i < _dimension; i++) {
+        temp += _coordinates[i] * _coordinates[i];
+    }
+    return (sqrt(temp));
+}
+
+
+int main()
+{
+    Vector<float> a(4, 2, 6);
+    Vector<float> b(4, 2, 6);
+    std::cout << "Vector a" << std::endl;
+    std::cout << a << std::endl;
+    std::cout << "Vector b" << std::endl;
+    std::cout << b << std::endl;
+    std::cout << "Area of triangle" << std::endl;
+    double abc = area(a,b);
+    std::cout << abc << std::endl;
+    return 0;
+}
